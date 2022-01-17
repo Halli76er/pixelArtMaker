1369da91c69bf699f5b7533c2452e6488b562a7e
Sven Fleshman <sven.fleshman@gmail.com>
2022-01-17 18:27:42 +0100

    update udacity review 1/2

--- designs.js
+++ designs.js
@@ -1,4 +1,3 @@
-
 // const Data
 const inputHeight = document.querySelector('#inputHeight');
 const inputWidth = document.querySelector('#inputWidth');
@@ -13,35 +12,35 @@ sizePicker.addEventListener('submit', makeGrid);
 
 // Function for EventListener
 function makeGrid(event) {
-event.preventDefault();
+	event.preventDefault();
 
-// Delete Grid
-while(pixelCanvas.firstChild){
-    pixelCanvas.removeChild(pixelCanvas.firstChild);
-}
+	// Delete Grid
+	while (pixelCanvas.firstChild) {
+		pixelCanvas.removeChild(pixelCanvas.firstChild);
+	}
 
-// Create Grid row and cell
-for (let i = 1; i <= inputHeight.value; i++){
-        const row = document.createElement('tr');
-        pixelCanvas.appendChild(row);
-    for(let j = 1; j <= inputWidth.value; j++){
-        const cell = document.createElement('td');
-        row.appendChild(cell);
-        }
-    }
+	// Create Grid row and cell
+	for (let i = 1; i <= inputHeight.value; i++) {
+		const row = document.createElement('tr');
+		pixelCanvas.appendChild(row);
+		for (let j = 1; j <= inputWidth.value; j++) {
+			const cell = document.createElement('td');
+			row.appendChild(cell);
+		}
+	}
 
-// new Grid in const 
-const tableCell = document.querySelectorAll('td');
+	// new Grid in const 
+	const tableCell = document.querySelectorAll('td');
 
-// Select color input
-tableCell.forEach( cell => {
-    // ADD EventListener colorPicker
-    cell.addEventListener('click', function(event) {
-        if (event.target.style.backgroundColor === ""){
-            event.target.style.backgroundColor = colorPicker.value;
-        }else{
-            event.target.style.backgroundColor = "";
-        }
-        });
-    });
+	// Select color input
+	tableCell.forEach(cell => {
+		// ADD EventListener colorPicker
+		cell.addEventListener('click', function(event) {
+			if (event.target.style.backgroundColor === "") {
+				event.target.style.backgroundColor = colorPicker.value;
+			} else {
+				event.target.style.backgroundColor = "";
+			}
+		});
+	});
 }
